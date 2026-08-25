ca6fbcf559d7a712d9482a8943954c8ad10fb54b
Yiming Guo <guoyiming78611@gmail.com>
2026-08-25 23:54:06 +0800

    save(郭一鸣): training log for 2026-08-25

---
+++ logs/郭一鸣/2026/08/25/0-solution.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int a[100010];
+int main()
+{
+    int n;cin>>n;
+    for(int i=1;i<=n;i++)cin>>a[i];
+    int ans=a[1];
+    for(int i=2;i<=n;i++)
+    {
+        if(a[i]>a[i-1])
+        {
+            ans+=a[i]-a[i-1];
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
