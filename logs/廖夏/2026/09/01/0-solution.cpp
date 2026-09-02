2df8df845539a7b26a915091f406ba0e47feb8d6
only_matthew <onlymatt@yeah.net>
2026-09-02 13:20:03 +0800

    save(廖夏): training log for 2026-09-01

--- logs/廖夏/2026/09/01/0-solution.cpp
+++ logs/廖夏/2026/09/01/0-solution.cpp
@@ -35,13 +35,29 @@ BigInt operator*(BigInt A, BigInt B){
     C.flatten(A.len + B.len + 1);
     return C;
 }
-BigInt operator/(BigInt A, long long b){  // 高精度 ÷ 小整数, 结果向下取整
-    BigInt C; C.init(0);
-    long long r = 0;
+BigInt operator-(BigInt A, BigInt B){  // 前提 A >= B
+    for (int i = 1; i <= A.len; i++) {
+        A[i] -= (i <= B.len ? B[i] : 0); // 天才般的想法！
+        if (A[i] < 0) { // 借位
+            A[i] += 10;
+            A[i + 1]--;
+        }
+    }
+    while (A.len > 1 && !A[A.len]) A.len--;
+    return A;
+}
+BigInt operator/(BigInt A, BigInt B){  // 高精度 ÷ 高精度, 向下取整
+    BigInt C, R;
+    C.init(0); // res
+    R.init(0); // 余数
     for (int i = A.len; i >= 1; i--) {
-        r = r * 10 + A[i];
-        C[i] = r / b;
-        r %= b;
+        for (int j = R.len; j >= 1; j--) R[j + 1] = R[j]; // 新概念乘 10
+        R.len++;
+        R[1] = A[i];
+        while (R.len > 1 && !R[R.len]) R.len--;
+        int q = 0;
+        while (!(R < B)) { R = R - B; q++; }
+        C[i] = q;
     }
     C.len = A.len;
     while (C.len > 1 && !C[C.len]) C.len--;
@@ -62,7 +78,8 @@ int main(){
     sum.init(a);
     ans.init(0);
     for (int i = 1; i <= n; i++) {
-        BigInt cur = sum / p[i].b;      // 第 i 个大臣的奖赏
+        BigInt dv; dv.init(p[i].b);
+        BigInt cur = sum / dv;      // 第 i 个大臣的奖赏
         if (ans < cur) ans = cur;
         BigInt tmp; tmp.init(p[i].a);
         sum = sum * tmp;
