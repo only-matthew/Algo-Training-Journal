fa8278ff848cd1ff554b13cb71ebbb480cf12b8c
wzzzzhhhhh <1989531784@qq.com>
2026-09-03 22:50:16 +0800

    save(王梓豪): training log for 2026-09-03

---
+++ logs/王梓豪/2026/09/03/0-solution.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{int n;
+	cin>>n;
+	vector<int> v(n);
+	for(int i=0;i<n;i++)
+	{cin>>v[i];
+	}
+	vector<int> dp(n);
+	dp[0]=1;
+	for(int i=1;i<n;i++)
+	{
+		for(int j=0;j<i;j++)
+		{
+			if(v[i]<=v[j])
+			{
+				dp[i]=max(dp[i],dp[j]+1);
+			}
+			else
+			{
+				dp[i]=max(dp[i],1);
+			}
+		}
+	}
+	int max_=-1;
+	for(int i=0;i<n;i++)
+	{
+		max_=max(max_,dp[i]);
+	}
+	cout<<max_;
+}
