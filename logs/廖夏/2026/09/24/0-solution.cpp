85c50ed5c157684fc7a98659b4755934bef938a8
only_matthew <onlymatt@yeah.net>
2026-09-25 01:39:26 +0800

    save(廖夏): training log for 2026-09-24

--- logs/廖夏/2026/09/24/0-solution.cpp
+++ logs/廖夏/2026/09/24/0-solution.cpp
@@ -1,40 +1,57 @@
 #include<iostream>
 #include<algorithm>
+#include<cmath>
+#include<vector>
 #include<queue>
-#include<cstring>
 using namespace std;
-const int MAXN = 405;
-int a[MAXN][MAXN];
-struct cord{
-    int x,y;
+struct node{
+    int floor, num;
 };
-int d[8][2] = {{1,2},{1,-2},{-1,2},{-1,-2},{2,1},{2,-1},{-2,1},{-2,-1}};
-queue<cord> q;
-int n, m, sx, sy;
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-    memset(a, -1, sizeof(a));
-    cin >> n >> m >> sx >> sy;
-    a[sx][sy] = 0;
-    q.push({sx,sy});
+    int n, a, b; cin >> n >> a >> b;
+    vector<int> k(n+5);
+    vector<bool> vis(n + n);
+    for (int i = 1; i <= n; i++) cin >> k[i];
+    queue<node> q; q.push({a,0});
+    int res = 1e9; vis[a] = true;
     while(!q.empty()){
-        cord u = q.front(); q.pop();
-        int ux = u.x, uy = u.y;
-        for(int i = 0; i < 8; i++){
-            int x_ = ux + d[i][0], y_ = uy + d[i][1]; 
-            if (x_ >= 1 && x_ <= n && y_ >= 1 && y_ <= m && a[x_][y_] == -1){
-                // cout << x_ <<" "<< y_ << endl;
-                q.push({x_,y_});
-                a[x_][y_] = a[ux][uy] + 1;
-            }
-        }
+        int floor = q.front().floor, num = q.front().num; q.pop();
+        if (floor == b) {res = min(res, num); continue;}
+        if (floor + k[floor] <= n && !vis[floor+k[floor]]) {vis[floor+k[floor]] = true;q.push({floor + k[floor], num+1});}
+        if (floor - k[floor] >= 1 && !vis[floor-k[floor]]) {vis[floor-k[floor]] = true;q.push({floor - k[floor], num + 1});}
     }
-    for (int i = 1; i <= n; i++){
-        for (int j = 1; j <= m; j++){
-            cout << a[i][j]; if (j < m) cout << " ";
-        }
-        cout << endl;
+    if (res == 1e9) cout << -1;
+    else cout << res;
+    return 0;
+}
+
+
+
+----------------------------分割线-------------------------------
+
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<vector>
+#include<queue>
+using namespace std;
+struct node{
+    int floor, num;
+};
+int main(){
+    int n, a, b; cin >> n >> a >> b;
+    vector<int> k(n+5);
+    vector<bool> vis(n + n);
+    for (int i = 1; i <= n; i++) cin >> k[i];
+    queue<node> q; q.push({a,0});
+    int res = 1e9; vis[a] = true;
+    while(!q.empty()){
+        int floor = q.front().floor, num = q.front().num; q.pop();
+        if (floor == b) {res = num; break;}
+        if (floor + k[floor] <= n && !vis[floor+k[floor]]) {vis[floor+k[floor]] = true;q.push({floor + k[floor], num+1});}
+        if (floor - k[floor] >= 1 && !vis[floor-k[floor]]) {vis[floor-k[floor]] = true;q.push({floor - k[floor], num + 1});}
     }
+    if (res == 1e9) cout << -1;
+    else cout << res;
     return 0;
 }
--- logs/廖夏/2026/09/24/0-takeaway.md
+++ logs/廖夏/2026/09/24/0-takeaway.md
@@ -1,1 +1,3 @@
-经典BFS，复习`!q.empty()`和d数组的用法
+一开始自己写出来的BFS还保留着DFS的答案更新习惯，没有意识到在这个无权BFS上，第一次到达必然是最短路（每个节点都同时延申出去一次，如果本次到达，同批次后面的不会比本次的更好）。
+
+因此，本轮的`if (floor == b) {res = min(res, num); continue;}`，完全可以改为`if (floor == b) {res=num; break;}`然后在外层进行`if （floor == b) cout << res; else cout << -1;`即可.
