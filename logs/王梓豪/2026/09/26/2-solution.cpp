228c6039e75beb7bce0a32acb5694d7678a204d5
wzzzzhhhhh <1989531784@qq.com>
2026-09-26 00:05:23 +0800

    save(王梓豪): training log for 2026-09-26

--- logs/王梓豪/2026/09/26/2-solution.cpp
+++ logs/王梓豪/2026/09/26/2-solution.cpp
@@ -1,1 +1,24 @@
-签到题不上传代码了
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;char ch;
+        string s;
+        cin>>n>>ch>>s;
+        int cnt=0;
+        for(int i=0;i<n/2;i++)
+        {
+            if(s[i]!=s[n-i-1])
+            {
+                cnt++;
+                if(s[i]!=ch&&s[n-1-i]!=ch)cnt++;
+            }
+        }
+        cout<<cnt<<endl;
+    }
+}
